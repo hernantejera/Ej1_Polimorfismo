560d0b6389baf379a3f5b0c149b5e170243ed0d1
hernantejera <hernanmtejera@gmail.com>
2023-07-15 13:07:08 -0300

    Agregar archivos de proyecto.

---
+++ Ej1_Polimorfismo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ej1_Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
