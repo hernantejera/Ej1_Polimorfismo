b61b475a338f9ac07a1bc9318ac4e337f25100cc
hernantejera <hernanmtejera@gmail.com>
2023-07-15 13:36:25 -0300

    se completo la tarea

--- Ej1_Polimorfismo/Program.cs
+++ Ej1_Polimorfismo/Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Perro perro = new Perro()
-            {
+            Perro perro = new Perro();
+            Console.WriteLine($"El perro hace {perro.HacerSonido()}"); 
 
-            };   
+            Gato gato = new Gato();
+            Console.WriteLine($"El gato hace {gato.HacerSonido()}");
+
+            Vaca vaca = new Vaca();
+            Console.WriteLine($"La vaca hace {vaca.HacerSonido()}");
         }
     }
 }
