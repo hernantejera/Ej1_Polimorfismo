f2026b5a12d02be015cf977e1eeca07a7a174d84
hernantejera <hernanmtejera@gmail.com>
2023-07-15 13:29:21 -0300

    creamos modelo

---
+++ Ej1_Polimorfismo/Perro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1_Polimorfismo
+{
+    public class Perro : ISonido
+    {
+        public string HacerSonido()
+        {
+            return "El perro hace guauuu guauuuu";
+        }
+    }
+}
---
+++ Ej1_Polimorfismo/Vaca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1_Polimorfismo
+{
+    public class Vaca : ISonido
+    {
+        public string HacerSonido()
+        {
+            return "La vaca lola hace muuuuuu";
+        }
+    }
+}
